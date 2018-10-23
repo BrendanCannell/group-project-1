245f465c7cd07567129b04d26b5ea16ead55a4ff
betonit007 <timnagorski@hotmail.com>
2018-10-23 16:03:31 -0400

    date forward back added

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
     var userSearchTerm;
     var year;
     var imgSrc;
+    var month;
+    var newDate;
+    var endDate
 
     
     $("#searchArticle").on("click", function(event) {
@@ -12,8 +15,30 @@ $(document).ready(function() {
 
         userSearchTerm = $("#searchInput").val().trim();
         year = parseInt($("#year").val());
-    
-        renderArticles();
+
+        if (userSearchTerm === "" || isNaN(year) || month == null)
+          {
+            $("#errorField").text("Please fill in all Search Fields");
+        }
+
+        else {
+            $("#errorField").empty();
+            year = parseInt($("#year").val());
+            newDate = year + month + "01";
+            newDate = newDate.toString();
+            endDate = moment(year + month + "01").endOf('month').format("YYYYMMDD");
+            endDate = endDate.toString();
+
+            renderArticles();
+        }
+    });
+
+    $(".dropdown-item").on("click", function(event) {
+        
+        event.preventDefault();
+
+        month = $(this).attr("data-mon");
+        $("#dropdownMonth").text(($(this).text()));
         
     });
 
@@ -22,9 +47,11 @@ $(document).ready(function() {
         event.preventDefault();
 
         year = parseInt($("#year").val());
+        if (isNaN(year)) {
+            year = 1999;
+        }
         year = year + 1; 
         $("#year").val(year);   
-        renderArticles();
         
     });
 
@@ -33,9 +60,12 @@ $(document).ready(function() {
         event.preventDefault();
 
         year = parseInt($("#year").val());
+        year = parseInt($("#year").val());
+        if (isNaN(year)) {
+            year = 2001;
+        }
         year = year - 1; 
         $("#year").val(year);   
-        renderArticles();
         
     });
 
@@ -55,50 +85,101 @@ $(document).ready(function() {
 
         if (userSearchTerm !== "" && year) {
 
-            $("#searchInput").val("");
+            if (year > 2005) {
+
 
                     var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
                     url += '?' + $.param({
                     'api-key': "b3319917b4b54c0ba4a7eb463d0099d9",
                     'q': userSearchTerm,
                     'fq': "news_desk:(\"Business\" \"Business Day\" \"Financial\" \"Your Money\" )",
-                    'begin_date':  year + "0101",
-                    'end_date':  year + "1231"
+                    'begin_date': newDate,
+                    'end_date': endDate
                     });
                     $.ajax({
                         url: url,
                         method: 'GET',
                     }).done(function(result) {
                         var test = result;
+
+                    if (test.response.docs.length === 0) {
+                        $("#errorField").text("No results found, please expand search.");  
+                    }
+
+                    var numberArticles = test.response.docs.length;
                 
-                    for (var i = 0; i < 10; i++) {
+                    for (var i = 0; i < numberArticles; i++) {
 
-                        if (test.response.docs[i].keywords.length !== 0) {
-                    
+                        var headlineLink = test.response.docs[i].web_url;
+                        var headliner = test.response.docs[i].headline.main;
+                        var imageCheck = test.response.docs[i].multimedia.length;
+                        var bodyText = test.response.docs[i].snippet;
+                        if (imageCheck) {
 
-                            var headlineLink = test.response.docs[i].web_url;
-                            var headliner = test.response.docs[i].headline.main;
-                            var imageCheck = test.response.docs[i].multimedia.length;
-                            var bodyText = test.response.docs[i].snippet;
-                            if (imageCheck) {
-
-                              imgSrc = "https://static01.nyt.com/" + test.response.docs[i].multimedia[2].url;
-                            }
-                            else {
-                              imgSrc = "assets/images/chart.png";
-                            }
-
-                            var articleCard = $("<div class='card articleCard'><div class='card-body'><h4 class='card-title'>" + headliner + "</h4><img class='rounded articleImg' src='" + imgSrc + "'</img><p class='card-text'>" + bodyText + "</p><a href='" + headlineLink + "' target='_blank' class='btn btn-primary toArticleButton'>See Full Article</a></div></div>");
-                
-                            $("#article-section").prepend(articleCard);
-                                
+                            imgSrc = "https://static01.nyt.com/" + test.response.docs[i].multimedia[0].url;
                         }
+                        else {
+                            imgSrc = "assets/images/chart.png";
+                        }
+
+                        var articleCard = $("<div class='card articleCard'><div class='card-body'><h4 class='card-title'>" + headliner + "</h4><img class='rounded articleImg' src='" + imgSrc + "'</img><p class='card-text'>" + bodyText + "</p><a href='" + headlineLink + "' target='_blank' class='btn btn-primary toArticleButton'>See Full Article</a></div></div>");
+            
+                        $("#article-section").prepend(articleCard);
+                        
                     }
                     
                 }).fail(function(err) {
                     throw err;
+                
                 });
+            }
+
+            if (year <= 2005) {
+
+ 
+                var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+                url += '?' + $.param({
+                  'api-key': "b3319917b4b54c0ba4a7eb463d0099d9",
+                  'q': userSearchTerm,
+                  'begin_date': newDate,
+                  'end_date': endDate
+                });
+                $.ajax({
+                  url: url,
+                  method: 'GET',
+                }).done(function(result) {
+                         var test = result;
+
+                if (test.response.docs.length === 0) {
+                $("#errorField").text("No results found, please expand search.");  
+                }
+ 
+                     var numberArticles = test.response.docs.length;
+                 
+                     for (var i = 0; i < numberArticles; i++) {
+                     
+ 
+                        var headlineLink = test.response.docs[i].web_url;
+                        var headliner = test.response.docs[i].headline.main;
+                
+                        var bodyText = test.response.docs[i].snippet;
+                        
+                        imgSrc = "assets/images/old.png";
+            
+                        var articleCard = $("<div class='card articleCard'><div class='card-body'><h4 class='card-title'>" + headliner + "</h4><img class='rounded articleImg' src='" + imgSrc + "'</img><p class='card-text'>" + bodyText + "</p><a href='" + headlineLink + "' target='_blank' class='btn btn-primary toArticleButton'>See Full Article</a></div></div>");
+            
+                        $("#article-section").prepend(articleCard);
+                                 
+                     }
+                     
+                 }).fail(function(err) {
+                     throw err;
+                 
+                 });
+             }
         }
+
+        
        
     }
 });
